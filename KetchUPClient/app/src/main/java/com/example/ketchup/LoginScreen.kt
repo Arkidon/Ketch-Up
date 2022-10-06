92e469595a3df291fb6a9a7802c41b3723dbcc21
Daviis00 <daviisclase@gmail.com>
2022-10-06 09:21:50 +0200

    prueba

--- KetchUPClient/app/src/main/java/com/example/ketchup/LoginScreen.kt
+++ KetchUPClient/app/src/main/java/com/example/ketchup/LoginScreen.kt
@@ -7,5 +7,7 @@ class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+
     }
+
 }
