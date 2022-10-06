b5ee054f59fe977d0669db03df6f8702f1eb6769
Daviis00 <daviisclase@gmail.com>
2022-10-06 16:37:32 +0200

    added login and register buttons

--- KetchUPClient/app/src/main/java/com/example/ketchup/LoginScreen.kt
+++ KetchUPClient/app/src/main/java/com/example/ketchup/LoginScreen.kt
@@ -1,12 +1,50 @@
 package com.example.ketchup
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginScreen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+        //login and register buttons
+        var loginButton: Button = findViewById(R.id.loginButton)
+        var registerButton: Button = findViewById(R.id.registerButton)
+        //Email and password texts
+        var emailText : EditText = findViewById(R.id.emailText)
+        var passwordText : EditText = findViewById(R.id.passwordText)
+
+        //takes to the chats selects
+        loginButton.setOnClickListener {
+
+            //checks if the fields are filled
+            if(emailText.text.isNotEmpty() && passwordText.text.isNotEmpty()) {
+                var intent: Intent
+                intent = Intent(this, ChatMenu::class.java)
+                startActivity(intent)
+             }
+            else if (emailText.text.isEmpty() && passwordText.text.isNotEmpty()) {
+                Toast.makeText(applicationContext, "You must provide an user ", Toast.LENGTH_SHORT).show()
+            }
+            else if (emailText.text.isNotEmpty() && passwordText.text.isEmpty()) {
+                Toast.makeText(applicationContext, "You must provide a password", Toast.LENGTH_SHORT).show()
+            }
+            else {
+            Toast.makeText(applicationContext, "You must provide an user and a password", Toast.LENGTH_SHORT).show()
+            }
+        }
+            //takes to the register screen
+            registerButton.setOnClickListener {
+            var intent: Intent
+            intent = Intent(this,  RegisterScreen::class.java)
+            startActivity(intent)
+        }
 
     }
 
--- KetchUPClient/app/src/main/java/com/example/ketchup/RegisterScreen.kt
+++ KetchUPClient/app/src/main/java/com/example/ketchup/RegisterScreen.kt
@@ -3,9 +3,10 @@ package com.example.ketchup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_screen)
+
     }
 }
