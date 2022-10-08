10e20376979ba05c834a668b60d6c96807b72edb
Daviis00 <daviisclase@gmail.com>
2022-10-08 23:14:38 +0200

    upgraded checks

--- KetchUPClient/app/src/main/java/com/ketchup/app/LoginScreen.kt
+++ KetchUPClient/app/src/main/java/com/ketchup/app/LoginScreen.kt
@@ -2,13 +2,16 @@ package com.ketchup.app
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class LoginScreen : AppCompatActivity() {
+        //var to make toasts
+        var toast: Toast? = null
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,26 +20,28 @@ class LoginScreen : AppCompatActivity() {
         var loginButton: Button = findViewById(R.id.loginButton)
         var registerButton: Button = findViewById(R.id.signupButton)
         //Email and password texts
-        var emailText : EditText = findViewById(R.id.usernameText)
+        var userText : EditText = findViewById(R.id.usernameText)
         var passwordText : EditText = findViewById(R.id.passwordField)
+
 
         //takes to the chats selects
         loginButton.setOnClickListener {
 
             //checks if the fields are filled
-            if(emailText.text.isNotEmpty() && passwordText.text.isNotEmpty()) {
+            if(userText.text.isNotEmpty() && passwordText.text.isNotEmpty()) {
                 var intent: Intent
                 intent = Intent(this, ChatMenu::class.java)
                 startActivity(intent)
              }
-            else if (emailText.text.isEmpty() && passwordText.text.isNotEmpty()) {
-                Toast.makeText(applicationContext, "You must provide an user ", Toast.LENGTH_SHORT).show()
+            else if (userText.text.isEmpty() && passwordText.text.isNotEmpty()) {
+                passwordText.text = null
+                showToast("You must provide an user", Toast.LENGTH_SHORT)
             }
-            else if (emailText.text.isNotEmpty() && passwordText.text.isEmpty()) {
-                Toast.makeText(applicationContext, "You must provide a password", Toast.LENGTH_SHORT).show()
+            else if (userText.text.isNotEmpty() && passwordText.text.isEmpty()) {
+                showToast("You must provide a password", Toast.LENGTH_SHORT)
             }
             else {
-            Toast.makeText(applicationContext, "You must provide an user and a password", Toast.LENGTH_SHORT).show()
+               showToast("You must provide an user and a password", Toast.LENGTH_SHORT)
             }
         }
             //takes to the register screen
@@ -47,5 +52,13 @@ class LoginScreen : AppCompatActivity() {
         }
 
     }
-
+    //function to check if toast is showing
+    fun showToast(text: CharSequence?, duration: Int) {
+        if (toast == null) toast = Toast.makeText(applicationContext, text, duration)
+        else{
+            toast?.cancel()
+            toast?.setText(text)
+        }
+        toast?.show()
+    }
 }
