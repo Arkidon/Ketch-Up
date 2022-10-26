44a77d8eef7e60542d35f20b8c6043eb77f9c072
Arkidon <arkidon850@gmail.com>
2022-10-26 22:11:26 +0200

    Parametrized max length values

--- MustardServer/app/models.py
+++ MustardServer/app/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.conf import settings
+
+
+class Sessions(models.Model):
+    session_id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey('Users', on_delete=models.DO_NOTHING)
+    session_token = models.CharField(max_length=32)
+    active = models.BooleanField()
 
 
 class Users(models.Model):
-    username = models.CharField(unique=True, max_length=25)
-    password = models.CharField(max_length=50)
-    user_id = models.BigAutoField
+    user_id = models.BigAutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=settings.USERNAME_MAX_LENGTH)
+    password = models.CharField(max_length=settings.PASSWORD_MAX_LENGTH)
+
+
