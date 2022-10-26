3d73633279e64b8b55ff028cbe0c652179f16c05
Arkidon <arkidon850@gmail.com>
2022-10-26 22:14:42 +0200

    Added the login view

--- MustardServer/app/views/login.py
+++ MustardServer/app/views/login.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, JsonResponse
+from json.decoder import JSONDecodeError
+from django.conf import settings
+from app.models import Users
+from app.decorators import session_required
+from app.utils import generate_session_token, validators
+import json
 
 
-# Create your views here.
-def login(request):
-    return HttpResponse("Let's go")
+@session_required.decorator
+def view(request, session):
+
+    credentials = validators.user_data_validation(request)
+
+    # Checks if the credentials have the correct format, if not, returns a 400 error code
+    if credentials is False:
+        return HttpResponse(status=400)
+
+    username = credentials['username']
+    password = credentials['password']
+
+    # Checks if the user exists
+    try:
+        user = Users.objects.get(username=username)
+
+    except ObjectDoesNotExist:
+        return HttpResponse(status=401)
+
+    # validates the password
+    if not check_password(password, user.password):
+        return HttpResponse(status=401)
+
+    # Genereates the session_token
+    session_token = generate_session_token.generate()
+
+    return JsonResponse({'session_id': session_token})
