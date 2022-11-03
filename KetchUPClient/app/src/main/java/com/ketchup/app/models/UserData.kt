7ebba9397ae390a61e54864c1dee41f0c74a7690
Daviis00 <daviisclase@gmail.com>
2022-11-03 23:26:59 +0100

    Changed view user classes from UserData to Users and removed UserData

--- KetchUPClient/app/src/main/java/com/ketchup/app/models/UserData.kt
+++
@@ -1,8 +0,0 @@
-package com.ketchup.app.models
-import android.graphics.Bitmap
-
-data class UserData(
-    val friendName:String,
-    val friendSub:String,
-    val pfp:Bitmap
-)
--- KetchUPClient/app/src/main/java/com/ketchup/app/view/ChatList.kt
+++ KetchUPClient/app/src/main/java/com/ketchup/app/view/ChatList.kt
@@ -1,7 +1,6 @@
 package com.ketchup.app.view
 
 import com.ketchup.app.models.ChatData
-import com.ketchup.app.models.UserData
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
