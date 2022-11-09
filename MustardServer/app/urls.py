56789c18037daffea15acad08a0c3f910600ae10
David <daviisclase@gmail.com>
2022-11-09 13:02:45 +0100

    Removed request user url and added a base implementation of the search users endpoint

--- MustardServer/app/urls.py
+++ MustardServer/app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import login, sign_up, request_users, ping_server
+from .views import login, sign_up, search_users, ping_server
 
 urlpatterns = [
     path('login', login.view),
     path('signup', sign_up.view),
-    path('request-users', request_users.view),
+    path('search-users', search_users.view),  # Receives URL parameters
     path('', ping_server.view)
 ]
