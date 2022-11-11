dacd5427f106431e819a0e391d50084253d6feac
Daviis00 <daviisclase@gmail.com>
2022-11-11 10:18:07 +0100

    Fixed main screen bug

--- KetchUPClient/app/src/main/java/com/ketchup/app/LoginScreen.kt
+++ KetchUPClient/app/src/main/java/com/ketchup/app/LoginScreen.kt
@@ -37,7 +37,7 @@ class LoginScreen : AppCompatActivity() {
         setContentView(R.layout.login_screen)
 
         // If url is default takes to dev activity
-        if( ServerAddress.readUrl(this).equals("http://127.0.0.1")){
+        if( ServerAddress.readUrl(this).equals("127.0.0.1")){
             startActivity(Intent(this, DevScreen::class.java))
         }
 
