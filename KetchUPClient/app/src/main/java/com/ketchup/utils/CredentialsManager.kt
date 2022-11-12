f778f54cf1161007d56d651efed3eadf6e561324
Arkidon <arkidon850@gmail.com>
2022-11-12 14:44:45 +0100

    Added a new utility to handle the credentials in the client

---
+++ KetchUPClient/app/src/main/java/com/ketchup/utils/CredentialsManager.kt
@@ -0,0 +1,40 @@
+package com.ketchup.utils
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import com.ketchup.app.KetchUp
+/**
+ * Manages the reading and writing of the user credentials used to validate
+ * against the server
+ */
+class CredentialsManager {
+    companion object{
+       // File name to store the preferences
+        private const val credentialsFile = "credentials"
+
+        /**
+         * @param key The name of the key to store
+         * @param value The value linked to the key
+         * @param activity An activity object to get the shared preferences
+         */
+        fun setCredential(key: String, value: String, activity: AppCompatActivity){
+            // Object to access the shared Preferences
+            val sharedPreferences = activity.getSharedPreferences(credentialsFile, Context.MODE_PRIVATE)
+
+            // Stores the
+            with(sharedPreferences.edit()){
+                putString(key, value)
+            }
+        }
+
+        /**
+         * @param key The name of the key to retrieve
+         * @param activity An activity object to get the shared preferences
+         * @return A String with the value linked to the specified key
+         */
+        fun getCredential(key: String, activity: AppCompatActivity): String{
+            val sharedPreferences = activity.getSharedPreferences(credentialsFile, Context.MODE_PRIVATE)
+            return sharedPreferences.getString(key, "").toString()
+        }
+    }
+}
