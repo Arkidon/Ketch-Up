7040c8f1d73c0c3a3e8e49bffcd04f536c88a32c
Arkidon <arkidon850@gmail.com>
2022-11-18 09:15:52 +0100

    Updated session_required decorator

--- MustardServer/app/decorators/session_required.py
+++ MustardServer/app/decorators/session_required.py
@@ -1,12 +1,22 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
-from app.models import Sessions
+from app.models import Sessions, Users
 
 
 def decorator(function):
+    """
+    Checks if the user has an active session, if there is no active session, returns a 401 error
+    """
+
     def wrapper(request):
+        # Checks if the necessary headers are present
+        if 'user' not in request.headers or 'session' not in request.headers:
+            return HttpResponse(status=400)
+
+        # Checks if the session exists
         try:
-            session = Sessions.objects.get(session_id=request.session.session, active=True)
+            user = Users.objects.get(user_id=request.headers["user"])
+            session = Sessions.objects.get(user=user, session_token=request.headers["session"], active=True)
 
         except ObjectDoesNotExist:
             return HttpResponse(status=401)
