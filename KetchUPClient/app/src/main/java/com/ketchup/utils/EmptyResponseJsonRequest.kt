63f95fa7d5f4a7e263743ce473b83796334b698c
Arkidon <arkidon850@gmail.com>
2022-11-19 21:01:05 +0100

    Added a new method to the EmptyResponseJsonRequest to support custom headers

--- KetchUPClient/app/src/main/java/com/ketchup/utils/EmptyResponseJsonRequest.kt
+++ KetchUPClient/app/src/main/java/com/ketchup/utils/EmptyResponseJsonRequest.kt
@@ -16,20 +16,21 @@ import org.json.JSONObject
 
 class EmptyResponseJsonRequest(method: Int, url: String, jsonRequest: JSONObject?, listener: Listener<JSONObject>,
                                errorListener: ErrorListener?) :
-    JsonObjectRequest(method, url,jsonRequest, listener, errorListener) {
+    JsonObjectRequest(method, url, jsonRequest, listener, errorListener) {
 
     /** Lock to guard mListener as it is cleared on cancel() and read on delivery.  */
-    private val mLock = Any()
+    private val lock = Any()
 
-    @GuardedBy("mLock")
-    private var mListener: Listener<String>? = null
+    private var headers = HashMap<String, String>()
+
+    @GuardedBy("lock")
+    private var listener: Listener<String>? = null
 
     @Override
     override fun parseNetworkResponse(response: NetworkResponse?): Response<JSONObject> {
         return if (response != null && response.data.isEmpty()) {
-            val jsonObject = JSONObject()
-
             // Dumb jsonObject
+            val jsonObject = JSONObject()
             jsonObject.put("empty", "empty")
 
             Response.success(jsonObject, HttpHeaderParser.parseCacheHeaders(response))
@@ -41,15 +42,27 @@ class EmptyResponseJsonRequest(method: Int, url: String, jsonRequest: JSONObject
     @Override
     override fun deliverResponse(response: JSONObject?) {
         var listener: Listener<String>?
-        synchronized(mLock) { listener = mListener}
+        synchronized(lock) { listener = this.listener }
         if (listener != null) {
             listener!!.onResponse(response.toString())
         }
     }
 
+    /**
+     * Adds custom headers to the request
+     */
+    fun addHeader(key: String, value: String){
+        headers[key] = value
+    }
+
+    @Override
+    override fun getHeaders(): MutableMap<String, String> {
+        return headers
+    }
+
     @Override
     override fun cancel() {
         super.cancel()
-        synchronized(mLock) { mListener = null }
+        synchronized(lock) { listener = null }
     }
 }
