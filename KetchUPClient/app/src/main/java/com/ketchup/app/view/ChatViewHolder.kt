95b1c0cd5d24d5699f2600d4b4fc9751ce9a06d0
Daviis00 <daviisclase@gmail.com>
2022-11-22 13:29:19 +0100

    Improved chat screen with sender and reciver colors

--- KetchUPClient/app/src/main/java/com/ketchup/app/view/ChatViewHolder.kt
+++ KetchUPClient/app/src/main/java/com/ketchup/app/view/ChatViewHolder.kt
@@ -1,12 +1,17 @@
 package com.ketchup.app.view
 
+import android.view.Gravity
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
+import com.ketchup.app.KetchUp
 import com.ketchup.app.R
 import com.ketchup.app.database.ChatEntries
+import com.ketchup.utils.files.CredentialsManager
 import java.time.LocalDate
-
 class ChatViewHolder (view: View): RecyclerView.ViewHolder(view){
     val messageText = view.findViewById<TextView>(R.id.textMessage)
     val messageDate = view.findViewById<TextView>(R.id.textDate)
@@ -17,5 +22,19 @@ class ChatViewHolder (view: View): RecyclerView.ViewHolder(view){
 
         // Placeholder date value
         messageDate.text = LocalDate.now().toString()
+        if (chatEntry.user_sender != CredentialsManager.getCredential("user", KetchUp.getCurrentActivity()).toInt()){
+                messageText.gravity= Gravity.START
+                messageText.background = AppCompatResources.getDrawable(KetchUp.getCurrentActivity(), R.drawable.background_recieved_message)
+                val textParams = messageText.layoutParams as ConstraintLayout.LayoutParams
+                textParams.endToEnd = ConstraintSet.UNSET
+
+                val dateParams = messageDate.layoutParams as ConstraintLayout.LayoutParams
+                dateParams.endToEnd = ConstraintSet.UNSET
+
+                messageText.requestLayout()
+                messageDate.requestLayout()
+
+        }
+
     }
 }
